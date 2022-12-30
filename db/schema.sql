6b16df28ebff0e38d69550022be31112245e1d37
McKay Fackrell <110206514+mckayfackrell@users.noreply.github.com>
2022-12-29 21:01:25 -0700

    added images assets

--- db/schema.sql
+++ db/schema.sql
@@ -1,10 +1,4 @@
 DROP DATABASE IF EXISTS transactions_db;
-//<<<<<<< feature/html-skeleton
-CREATE DATABASE transactions_db;
-
-=======
-
 CREATE DATABASE transactions_db;
 
 USE transactions_db;
-//>>>>>>> main
