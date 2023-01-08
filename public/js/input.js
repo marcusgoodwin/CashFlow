f5ee4be2f98e92906c25e2ad799bd8f15c410dcf
JHoang <JHoang6900@gmail.com>
2023-01-07 22:49:13 -0700

    subtraction from big number complete

--- public/js/input.js
+++ public/js/input.js
@@ -11,8 +11,10 @@ const column5 = [];
 function addData() {
   let addRowX = document.getElementById("transactions_table");
   let NewRow = addRowX.insertRow(x);
+  let bigNumberString = document.getElementById("big_number").innerHTML;
 
-  let bigNumber = parseInt(document.getElementById("big_number").innerHTML)
+  let bigNumber = parseInt(bigNumberString.replace('$', ''));
+
 
   column1[y] = document.getElementById("purchase_date").value;
   column2[y] = parseInt(document.getElementById("price").value);
@@ -26,11 +28,15 @@ function addData() {
   let cell4 = NewRow.insertCell(3);
   let cell5 = NewRow.insertCell(4);
 
+  let result = bigNumber - column2[y];
+
+
   cell1.innerHTML = column1[y];
   cell2.innerHTML = '$' + column2[y];
   cell3.innerHTML = column3[y];
   cell4.innerHTML = column4[y];
   cell5.innerHTML = column5[y];
+
 
 
 
@@ -43,7 +49,11 @@ function addData() {
   console.log('typeof column2[y] ~>',typeof column2[y])
 
   console.log('bigNumber-column2[y] ~>', bigNumber - column2[y])
-  document.getElementById("big_number").innerHTML = bigNumber-column2[y]
+  
+  let resultString = '$' + result.toFixed(2);
+  document.getElementById("big_number").innerHTML = resultString;
+
+
 
   console.log('column2[y] pt2 ~>', column2[y])
   console.log('bigNumber pt2 ~>',bigNumber);
@@ -51,7 +61,7 @@ function addData() {
   x++;
   y++;
 
-   
+  
 }
 
 // I was trying to clear the input cells after the submit button was clicked
