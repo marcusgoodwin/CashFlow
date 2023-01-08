868023574c575783c9e25eacc2cfa7afe05538de
JHoang <JHoang6900@gmail.com>
2023-01-07 21:56:23 -0700

    subtracts from big number (no dollarsign)

--- public/js/input.js
+++ public/js/input.js
@@ -1,37 +1,58 @@
-    var x = 1;
-		var y = 0;
+var x = 1;
+var y = 0;
 
-    var column1 = [];
-		var column2 = [];
-		var column3 = [];
-		var column4 = [];
-    var column5 = [];
+const column1 = [];
+const column2 = [];
+const column3 = [];
+const column4 = [];
+const column5 = [];
 
-    function addData(){
-			var addRowX = document.getElementById('transactions_table');
-			var NewRow = addRowX.insertRow(x);
 
-			column1[y] = document.getElementById("purchase_date").value;
-			column2[y] = document.getElementById("price").value;
-			column3[y] = document.getElementById("description").value;
-			column4[y] = document.getElementById("merchant").value;
-      column5[y] = document.getElementById("category").value;
+function addData() {
+  let addRowX = document.getElementById("transactions_table");
+  let NewRow = addRowX.insertRow(x);
 
-			var cell1 = NewRow.insertCell(0);
-			var cell2 = NewRow.insertCell(1);
-			var cell3 = NewRow.insertCell(2);
-			var cell4 = NewRow.insertCell(3);
-      var cell5 = NewRow.insertCell(4);
+  let bigNumber = parseInt(document.getElementById("big_number").innerHTML)
 
-			cell1.innerHTML = column1[y];
-			cell2.innerHTML = '$'+column2[y];
-			cell3.innerHTML = column3[y];
-			cell4.innerHTML = column4[y];
-    	cell5.innerHTML = column5[y];
+  column1[y] = document.getElementById("purchase_date").value;
+  column2[y] = parseInt(document.getElementById("price").value);
+  column3[y] = document.getElementById("description").value;
+  column4[y] = document.getElementById("merchant").value;
+  column5[y] = document.getElementById("category").value;
 
-			x++;
-			y++;
-		}
+  let cell1 = NewRow.insertCell(0);
+  let cell2 = NewRow.insertCell(1);
+  let cell3 = NewRow.insertCell(2);
+  let cell4 = NewRow.insertCell(3);
+  let cell5 = NewRow.insertCell(4);
+
+  cell1.innerHTML = column1[y];
+  cell2.innerHTML = '$' + column2[y];
+  cell3.innerHTML = column3[y];
+  cell4.innerHTML = column4[y];
+  cell5.innerHTML = column5[y];
+
+
+
+  console.log('column2[y] ~>', column2[y])
+  console.log('bigNumber ~>',bigNumber);
+  
+  console.log('===========')
+
+  console.log('typeof bigNumber ~>',typeof bigNumber)
+  console.log('typeof column2[y] ~>',typeof column2[y])
+
+  console.log('bigNumber-column2[y] ~>', bigNumber - column2[y])
+  document.getElementById("big_number").innerHTML = bigNumber-column2[y]
+
+  console.log('column2[y] pt2 ~>', column2[y])
+  console.log('bigNumber pt2 ~>',bigNumber);
+
+  x++;
+  y++;
+
+   
+}
 
 // I was trying to clear the input cells after the submit button was clicked
 
@@ -43,8 +64,6 @@
 //     input.value = '';
 //   });
 // });
-
-		
 
 // purchase_date
 // price
